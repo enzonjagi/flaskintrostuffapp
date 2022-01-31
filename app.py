5ba9bb335df78c6c517e3b1d59073e9b2ec72b01
enzonjagi <amosndungo@gmail.com>
2022-01-31 11:05:13 +0300

    Debug app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from sqlalchemy import Column
-from sympy import content
+# from sympy import content
 from werkzeug.utils import escape, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
